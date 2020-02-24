50925b5e5892f550e46aaf07c813e365e04d5de2
Hayden Hudson <hhudson@insum.ca>
2020-02-24 17:08:12 -0500

    latst

---
+++ material-components-web-codelabs-master/mdc-101/starter/login.js
@@ -0,0 +1,4 @@
+import { MDCTextField } from "@material/textfield";
+
+const username = new MDCTextField(document.querySelector(".username"));
+const password = new MDCTextField(document.querySelector(".password"));
